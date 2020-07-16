024e8aa977d9bdbccfd622ec99e5727e61deacda
Camrynne Sullivan <camrynne.sullivan@gmail.com>
2020-07-16 15:01:17 -0400

    Created working State Code API Click Event

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
 // Geolocation
+
+var stateJokeBtn = $("#regional");
 // When a user clicks a button
 
-var regionalJokes = $("#regional-joke");
+var apiKey = "e4a497ac65d9804f1a154098c8025426";
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    regionalJokes.text("You are not found joke");
-  }
-}
+var queryURL = "http://api.ipstack.com/68.174.9.216?access_key=" + apiKey;
 
-var apiKey = "bbfa444193f64b9eb5224b986b66c7e3";
-
-var queryURL =
-  "https://api.opencagedata.com/geocode/v1/json?q=LAT+LNG&key=" + apiKey;
-// //
-// function showPosition(position) {
-//   console.log(position);
-//   regionalJokes.text(
-//     "Latitude: " +
-//       position.coords.latitude +
-//       "<br>Longitude: " +
-//       position.coords.longitude
-//   );
-// }
-
-getLocation();
-
-console.log(getLocation);
-
-// Create an event listener
-$("#regional").click(getLocation);
+stateJokeBtn.on("click", function () {
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    // State Name
+    var stateName = response.region_code;
+    $(".regional-joke").text(stateName);
+    console.log("State Name:", stateName);
+  });
+});
 
 // if the user is in this region, coordinates with joke appear
 // else if the user does not have coordinates a different joke appears
