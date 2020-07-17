27e194830fdc2456ab5f8983d79d33fa39d60db1
Camrynne Sullivan <camrynne.sullivan@gmail.com>
2020-07-17 14:26:53 -0400

    Added Silly Fart Button

--- sounds.js
+++ sounds.js
@@ -2,6 +2,13 @@
 var commonFartSound = new Audio();
 commonFartSound.src = "Fart-Common-Everyday-Fart_Mike-Koenig.wav";
 
-function PlaySound() {
+function PlayCommonFartSound() {
   commonFartSound.play();
+}
+
+var sillyFartSound = new Audio();
+sillyFartSound.src = "Silly_Farts-Joe-1473367952.wav";
+
+function PlaySillyFartSound() {
+  sillyFartSound.play();
 }
