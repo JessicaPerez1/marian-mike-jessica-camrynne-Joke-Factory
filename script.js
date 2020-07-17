e4b0a49cc804bb852489e7bf1ce585c21b90d9b2
Marian Gude <mkgude@gmail.com>
2020-07-16 22:26:36 -0500

    made updates to script.js and added divs to html for hh-joke section

--- script.js
+++ script.js
@@ -23,3 +23,97 @@ stateJokeBtn.on("click", function () {
 // if the user is in this region, coordinates with joke appear
 // else if the user does not have coordinates a different joke appears
 // Then a joke correlated to their location pops up
+var today = new Date();
+// Wed Jul 15 2020 18:44:34 GMT-0500 (Central Daylight Time)
+var time = today.getHours() + ":" + today.getMinutes();
+console.log(time);
+var date =
+  today.getMonth() +
+  "/" +
+  today.getDate() +
+  "/" +
+  today.getFullYear() +
+  "  " +
+  time;
+$("#date-time").text(date);
+var dirtyJoke = [
+  {
+    question: "What did Cinderella do when she arrived at the ball?",
+    answer: "She gagged",
+  },
+  {
+    question: "What’s the difference between a tire and 365 used condoms?",
+    answer: "One’s a Goodyear. The other’s a great year.",
+  },
+  {
+    question: "What do you call the useless piece of skin on a dick?",
+    answer: "The man.",
+  },
+  {
+    question: "Why did the sperm cross the road?",
+    answer: "Because I put on the wrong sock this morning.",
+  },
+  {
+    question: "What does one boob say to the other boob?",
+    answer: "If we don’t get support, people will think we’re nuts.",
+  },
+  {
+    question: "How do you make your husband scream during sex?",
+    answer: " Call him and let him hear it.",
+  },
+  {
+    question: "What does the sign on an out-of-business brothel say?",
+    answer: "Beat it. We’re closed.",
+  },
+  {
+    question: "What do you call a lesbian dinosaur?",
+    answer: "Lick-a-lotta-puss.",
+  },
+  {
+    question: "What do a penis and a Rubik’s Cubes have in common?",
+    answer: "The more you play with it, the harder it gets.",
+  },
+  {
+    question: "What’s the best part about gardening?",
+    answer: "Getting down and dirty with your hoes.",
+  },
+];
+
+// for happy hour we will display a 'dirty' joke
+
+// during 3pm-6pm add a button to the page that displays a 'dirty' joke.
+
+$("#btn-happy-hour").on("click", function () {
+  var randomDirtyJoke = dirtyJoke[Math.floor(Math.random() * dirtyJoke.length)];
+  console.log(randomDirtyJoke);
+  var randomQuestion = JSON.stringify(randomDirtyJoke.question);
+  console.log(randomQuestion);
+  var randomAnswer = JSON.stringify(randomDirtyJoke.answer);
+  console.log(randomAnswer);
+  $("#hh-question").html(randomQuestion.replace(/['"]+/g, ""));
+  $("#hh-answer").html(randomAnswer.replace(/['"]+/g, ""));
+});
+
+// div for the hh container #hh-container
+//  when it is happy hour add a div to the page
+function hhJokeDisplay() {
+  //  if 3pm-6pm display div
+  var startTime = new Date().setHours(15);
+  console.log(startTime);
+  var endTime = new Date().setHours(23);
+  console.log(endTime);
+  if (today.getHours() < startTime && today.getHours() > endTime) {
+    $("#happy-hour-display-cont").hide();
+    $("#not-happy-hour").show();
+    $("#btn-happy-hour").hide();
+  }
+  //    On div have a button
+  //    When button selected, display one joke from the array
+  //  else hide div
+  else {
+    $("#happy-hour-display-cont").show();
+    $("#not-happy-hour").hide();
+    $("#btn-happy-hour").show();
+  }
+}
+hhJokeDisplay();
